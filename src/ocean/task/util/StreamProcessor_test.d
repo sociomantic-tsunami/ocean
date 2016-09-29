120273c2b345fa9d51562927317ae262c854b971
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2016-09-29 16:48:40 +0200

    Add license to new task package
    Files in this new package were missing the license information and also
    have an outdated copyright notice.

--- src/ocean/task/util/StreamProcessor_test.d
+++ src/ocean/task/util/StreamProcessor_test.d
@@ -3,7 +3,14 @@
     Example implementation/usage of stream processor which does no I/O and thus
     can is also used as a unit test
 
-    Copyright: Copyright (c) 2016 sociomantic labs. All rights reserved
+    Copyright:
+        Copyright (c) 2009-2016 Sociomantic Labs GmbH.
+        All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE_BOOST.txt for details.
+        Alternatively, this file may be distributed under the terms of the Tango
+        3-Clause BSD License (see LICENSE_BSD.txt for details).
 
 *******************************************************************************/
 
--- src/ocean/task/util/Timer.d
+++ src/ocean/task/util/Timer.d
@@ -9,7 +9,14 @@
     Usage example:
         See the documented unittest of the `wait()` function
 
-    Copyright: Copyright (c) 2016 sociomantic labs. All rights reserved
+    Copyright:
+        Copyright (c) 2009-2016 Sociomantic Labs GmbH.
+        All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE_BOOST.txt for details.
+        Alternatively, this file may be distributed under the terms of the Tango
+        3-Clause BSD License (see LICENSE_BSD.txt for details).
 
 *******************************************************************************/
 
