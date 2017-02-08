360caa2c315fe1c8b196958a88a04bab37034e8f
Leandro Lucarella <leandro.lucarella@sociomantic.com>
2017-02-08 18:14:26 +0100

    travis: Fix docker image naming
    The name for the docker image for PRs was wrongly set to :ci-false for
    push builds. The script to get the name was greatly simplified as since
    PRs can't really push images, we only always have to use the (base)
    branch image as a cache.

--- ci/travis-image-name.sh
+++ ci/travis-image-name.sh
@@ -1,10 +1,2 @@
 #!/bin/sh
-
-# Build docker image name
-img="sociomantictsunami/$(basename $TRAVIS_REPO_SLUG):ci"
-if test "$TRAVIS_PULL_REQUEST" != "false"
-then
-    echo "$img-$TRAVIS_BRANCH"
-else
-    echo "$img-$TRAVIS_PULL_REQUEST"
-fi
+echo "sociomantictsunami/$(basename $TRAVIS_REPO_SLUG):ci-$TRAVIS_BRANCH"
