469b0efcb66c2f0cd5845b2b6fc6df7322c0aab5
Mathias Lang <mathias.lang@sociomantic.com>
2017-02-08 19:12:35 +0100

    Add a test to FlexibleFileQueue for reopening it

--- test/flexiblefilequeue/main.d
+++ test/flexiblefilequeue/main.d
@@ -27,29 +27,60 @@ public void main ()
 
     const istring test_file = "testfile";
 
+
+    static void pushItems (FlexibleFileQueue queue, size_t size)
+    {
+        for (ubyte i = 0; i < size; i++)
+        {
+            auto item = [i, cast(ubyte) (ubyte.max-i), i, cast(ubyte) (i * i)];
+            assert(queue.push(item), "push failed");
+        }
+    }
+
+    static void popItems (FlexibleFileQueue queue, size_t size)
+    {
+        for (ubyte i = 0; i < size; i++)
+        {
+            auto pop = queue.pop;
+            auto item = [i, ubyte.max - i, i, cast(ubyte) (i * i)];
+            assert(pop == item, "pop failed " ~ pop.stringof);
+        }
+    }
+
     void doTest (bool open_existing)
     {
         for (ubyte size; size < ubyte.max; size++)
         {
             auto queue = new FlexibleFileQueue(test_file, 4, open_existing);
-
-            for (ubyte i = 0; i < size; i++)
-            {
-                auto item = [i, cast(ubyte) (ubyte.max-i), i, cast(ubyte) (i * i)];
-                assert(queue.push(item), "push failed");
-            }
-
-            for (ubyte i = 0; i < size; i++)
-            {
-                auto pop = queue.pop;
-                auto item = [i, ubyte.max - i, i, cast(ubyte) (i * i)];
-                assert(pop == item, "pop failed " ~ pop.stringof);
-            }
-
+            pushItems(queue, size);
+            popItems(queue, size);
             queue.clear();
         }
     }
 
     doTest(false);
     doTest(true);
+
+    void reopenTestWrite ()
+    {
+        scope queue = new FlexibleFileQueue(test_file, 4, true);
+        assert(queue.is_empty());
+        assert(queue.length() == 0);
+        pushItems(queue, 100);
+        assert(!queue.is_empty());
+        assert(queue.length() == 100);
+    }
+
+    void reopenTestRead ()
+    {
+        scope queue = new FlexibleFileQueue(test_file, 4, true);
+        assert(!queue.is_empty());
+        assert(queue.length() == 100);
+        popItems(queue, 100);
+        assert(queue.is_empty());
+        assert(queue.length() == 0);
+    }
+
+    reopenTestWrite();
+    reopenTestRead();
 }
