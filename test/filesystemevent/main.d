b70e6745f8671926930070933f03593105e71b65
Nemanja Boric <nemanja.boric@sociomantic.com>
2017-03-17 15:30:35 +0100

    Pass SmartUnion to FileSystemEvent's callbacks.
    `FileSystemEvent`'s constructor and `setNotifier` now accepts
    `FileSystemEvent.Notifier` which accepts the SmartUnion of the possible
    events that can be generated
    (with one union member carrying the name as well as the path).
    
    This is useful for monitoring the directories, where the name of the affected
    file will be passed to this handler.
    
    In addition, the old Handler is deprecated.

--- test/filesystemevent/main.d
+++ test/filesystemevent/main.d
@@ -15,8 +15,6 @@
 
 module test.filesystemevent.main;
 
-import ocean.text.convert.Formatter;
-
 
 /*******************************************************************************
 
@@ -50,7 +48,7 @@ import ocean.task.Task;
 
 import ocean.task.Scheduler;
 
-class FileModificationTestTask: Task
+deprecated class FileModificationTestTask: Task
 {
     /***************************************************************************
 
@@ -174,8 +172,222 @@ class FileModificationTestTask: Task
                     break;
 
                 default:
-                    test(false, format("Unexpected file system event notification. {}", event));
+                    test(false, "Unexpected file system event notification.");
             }
+        }
+    }
+}
+
+class FileCreationTestTask: Task
+{
+    /***************************************************************************
+
+        File operations to be checked
+
+    ***************************************************************************/
+
+    private bool created;
+    private bool modified = false;
+    private bool deleted  = false;
+    private bool closed   = false;
+
+    /***************************************************************************
+
+        Name of the created file.
+
+    ***************************************************************************/
+
+    private cstring created_name;
+
+    /***************************************************************************
+
+        Path to the monitored file.
+
+    ***************************************************************************/
+
+    private FilePath temp_path;
+
+    /***************************************************************************
+
+        Variable to control/test the order of the file operations
+
+    ***************************************************************************/
+
+    private int operation_order = 0;
+
+    /***************************************************************************
+
+        Path to the monitored directory.
+
+    ***************************************************************************/
+
+    private cstring watched_path;
+
+    /***************************************************************************
+
+        Tested FileSystemEvent instance.
+
+    ***************************************************************************/
+
+    private FileSystemEvent inotifier;
+
+    /***************************************************************************
+
+        Test that tests monitoring a directory and watching for the file
+        creation.
+
+    ***************************************************************************/
+
+    private void testFileCreation ( )
+    {
+        inotifier.watch(this.watched_path.dup,
+               FileEventsEnum.IN_CREATE);
+
+        theScheduler.epoll.register(inotifier);
+
+        auto file_name = "test_file";
+        File.set(file_name, "".dup);
+
+        this.suspend();
+
+        theScheduler.epoll.unregister(inotifier);
+
+        test(this.created);
+        test!("==")(this.created_name, file_name);
+    }
+
+    /***************************************************************************
+
+        Test that tests modifications/closing/deleting performed on individual
+        file (not a directory)
+
+    ***************************************************************************/
+
+    private void testFileModification ( )
+    {
+        auto temp_file = new TempFile(TempFile.Permanent);
+        this.temp_path = FilePath(temp_file.toString());
+
+        inotifier.watch(cast(char[]) temp_file.toString(),
+                       FileEventsEnum.IN_MODIFY | FileEventsEnum.IN_DELETE_SELF
+                     | FileEventsEnum.IN_CLOSE_WRITE );
+
+        theScheduler.epoll.register(inotifier);
+
+        temp_file.write("something");
+        temp_file.close;
+        temp_path.remove();
+
+        this.suspend();
+
+        theScheduler.epoll.unregister(inotifier);
+
+        test(this.modified);
+        test(this.closed);
+        test(this.deleted);
+    }
+
+    /***************************************************************************
+
+        Test entry point. Prepares environment and tests the FileSystemEvent.
+
+    ***************************************************************************/
+
+    override public void run ( )
+    {
+        auto sandbox = DirectorySandbox.create();
+        scope (exit)
+            sandbox.exitSandbox();
+
+        this.watched_path = sandbox.path;
+
+        this.inotifier  = new FileSystemEvent(&this.fileSystemHandler);
+
+        this.testFileCreation();
+        this.testFileModification();
+    }
+
+    /**********************************************************************
+
+        File System handler: called anytime a File System event occurs.
+
+        Params:
+            path   = monitored path
+            name = name of the file
+            event  = Inotify event (see FileEventsEnum)
+
+    **********************************************************************/
+
+    private void fileSystemHandler ( FileSystemEvent.RaisedEvent raised_event )
+    {
+        with (raised_event.Active) switch (raised_event.active)
+        {
+        case directory_file_event:
+            auto event = raised_event.directory_file_event;
+
+            if ( this.watched_path == event.path )
+            {
+                switch ( event.event )
+                {
+                    case FileEventsEnum.IN_CREATE:
+                        this.created = true;
+                        this.created_name = event.name.dup;
+                        if (this.suspended())
+                            this.resume();
+                        break;
+                    default:
+                        test(false, "Unexpected file system event notification.");
+                }
+            }
+            break;
+
+        case file_event:
+            auto event = raised_event.file_event;
+
+            if ( this.temp_path == event.path )
+            {
+                this.operation_order++;
+
+                switch ( event.event )
+                {
+                    case FileEventsEnum.IN_MODIFY:
+
+                        if ( this.operation_order == 1 )
+                        {
+                            this.modified = true;
+                        }
+                        break;
+
+                    case FileEventsEnum.IN_CLOSE_WRITE:
+
+                        if ( this.operation_order == 2 )
+                        {
+                            this.closed = true;
+                        }
+                        break;
+
+                    case FileEventsEnum.IN_DELETE_SELF:
+
+                        if ( this.operation_order == 3 )
+                        {
+                            this.deleted = true;
+                            if (this.suspended())
+                                this.resume();
+                        }
+                        break;
+
+                    case FileEventsEnum.IN_IGNORED:
+                        enforce(this.deleted);
+                        break;
+
+                    default:
+                        test(false, "Unexpected file system event notification.");
+                }
+            }
+            break;
+
+        default:
+            assert(false);
         }
     }
 }
@@ -204,8 +416,19 @@ unittest
         throw e;
     };
 
+    auto dir_test_task = new FileCreationTestTask;
+    theScheduler.schedule(dir_test_task);
+    theScheduler.eventLoop();
+}
+
+deprecated unittest
+{
+    initScheduler(SchedulerConfiguration.init);
+    theScheduler.exception_handler = (Task t, Exception e) {
+        throw e;
+    };
+
     auto test_task = new FileModificationTestTask;
     theScheduler.schedule(test_task);
-
     theScheduler.eventLoop();
 }
