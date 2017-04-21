92fef4187801f339d957794942e3498748676841
davideckardt <david.eckardt@sociomantic.com>
2017-04-21 17:27:05 +0200

    Collate `enforceInputSize` calls

--- src/ocean/util/serialize/contiguous/Deserializer.d
+++ src/ocean/util/serialize/contiguous/Deserializer.d
@@ -556,22 +556,17 @@ struct Deserializer
                     `);
 
 
+                This.e.enforceInputSize!(S)(data.length, pos);
 
                 static if (is (Element[] == RejectQualifier!(Field)))
                 {
                     // dynamic array
-
-                    This.e.enforceInputSize!(S)(data.length, pos);
-
                     pos += This.countDynamicArraySize!(Element)(
                         data[pos .. $], extra_bytes);
                 }
                 else static if (hasIndirections!(Element))
                 {
                     // static array with indirections
-
-                    This.e.enforceInputSize!(S)(data.length, pos);
-
                     pos += This.countArraySize!(Element)(
                         Field.length, data[pos .. $], extra_bytes);
                 }
@@ -887,10 +882,10 @@ struct Deserializer
             {
                 // To support const substitute Field with Unqual!(Field) in this
                 // scope.
+                This.e.enforceInputSize!(S)(data.length, pos);
+
                 static if (is (Element[] == Field))
                 {
-                    This.e.enforceInputSize!(S)(data.length, pos);
-
                     auto increment = This.sliceArray(field, data[pos .. $],
                         slices_buffer);
 
@@ -909,8 +904,6 @@ struct Deserializer
                 }
                 else static if (hasIndirections!(Element))
                 {
-                    This.e.enforceInputSize!(S)(data.length, pos);
-
                     pos += This.sliceSubArrays(field, data[pos .. $], slices_buffer);
                 }
             }
