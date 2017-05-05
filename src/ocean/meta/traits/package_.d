e43207db6fb08ba131f274edb1b37e08ad3f6af5
Mihails Strasuns <mihails.strasuns.contractor@sociomantic.com>
2017-05-05 10:31:40 +0200

    Define most basic set of type traits
    The `Basic` module contains basic non-recursive type traits (mostly adapted from
    old `ocean.core.Traits_tango` module) that are needed to distinguish between
    different kinds of types.

---
+++ src/ocean/meta/traits/package_.d
@@ -0,0 +1,20 @@
+/*******************************************************************************
+
+    This package contains various template utilities that allow to reason about
+    types in some way. Most traits answer simple boolean question about
+    parameter type having certain qualities ("traits") thus the name.
+
+    Copyright:
+        Copyright (c) 2017 Sociomantic Labs GmbH.
+        All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE_BOOST.txt for details.
+        Alternatively, this file may be distributed under the terms of the Tango
+        3-Clause BSD License (see LICENSE_BSD.txt for details).
+
+*******************************************************************************/
+
+module ocean.meta.traits.package_;
+
+public import ocean.meta.traits.Basic;
