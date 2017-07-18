sociomantic-tsunami/ocean

commit fd813adfc91c0f56616577d6706db4899cce7917

Decode the base64 with the missing padding
This makes base64 work when there not all padding bytes needed are in
the input string. It decodes as many whole non-padded quartets as it
can, and then it tries to decode the last remaining bytes into a pair,
or a single character.

Fixes #155

src/ocean/util/encode/Base64.d
@@ -317,6 +317,12 @@ body
     unique. This allows the use of decode tables with alternatives for certain
     characters.
 
+    Since the padding is required only between multiple base64 strings (for the
+    decoder not to interpret the start of the next string as the end of the
+    previous one), data may or may not contain the trailing padding, and it will
+    still be decoded correctly.
+
+
     Params:
       Table = The decode table to use. Variadic template parameter is used to
               allow passing it as an expression in D1, but a single `ubyte[256]`
@@ -357,6 +363,11 @@ body
     Note that the entries in the provided decode table are not required to be
     unique. This allows the use of decode tables with alternatives for certain
     characters.
+
+    Since the padding is required only between multiple base64 strings (for the
+    decoder not to interpret the start of the next string as the end of the
+    previous one), data may or may not contain the trailing padding, and it will
+    still be decoded correctly.
 
     Params:
       Table = The decode table to use. Variadic template parameter is used to
@@ -422,8 +433,8 @@ body
 
         if (padCount > 2)
             throw new Exception("Improperly terminated base64 string. Base64 pad character (=) found where there shouldn't be one.");
-        if (padCount == 0)
-            paddedPos = 0;
+        if (padCount < 2)
+            paddedPos = cast(ubyte)((data.length-padCount) % 4);
 
         auto nonPadded = data[0..($ - paddedPos)];
         foreach(piece; nonPadded)
@@ -446,12 +457,17 @@ body
         if (paddedPos)
         {
             auto padded = data[($ - paddedPos) .. $];
-            foreach(char piece; padded)
+            assert (padded.length <= 4);
+
+            // Fill the missing paddings if needed
+            base64Quad[padded.length .. $] = BASE64_PAD;
+
+            foreach(i, char piece; padded)
             {
                 ubyte next = table[piece];
                 if (next || piece == 'A')
                     *quadPtr++ = next;
-                if (quadPtr is endPtr)
+                if (i == padded.length - 1)
                 {
                     *rtnPt++ = cast(ubyte) (((base64Quad[0] << 2) | (base64Quad[1]) >> 4));
                     if (base64Quad[2] != BASE64_PAD)
@@ -507,6 +523,35 @@ unittest
         test!("==")(result, payload);
         result = decode("SGVsbG8sIGhvdyBhcmUgeW91IHRvZGF5Pw==");
         test!("==")(result, payload);
+        result = decode("SGVsbG8sIGhvdyBhcmUgeW91IHRvZGF5Pw=");
+        test!("==")(result, payload);
+        result = decode("SGVsbG8sIGhvdyBhcmUgeW91IHRvZGF5Pw");
+        test!("==")(result, payload);
+    }
+
+    // single padding test
+    {
+        // try the string with the single `=` padding, and without it
+        auto result = cast(Const!(ubyte)[])"any carnal pleasure.";
+        test!("==")(result, decode("YW55IGNhcm5hbCBwbGVhc3VyZS4="));
+        test!("==")(result, decode("YW55IGNhcm5hbCBwbGVhc3VyZS4"));
+    }
+
+    // short decode test, double-= padding
+    {
+        auto result = cast(Const!(ubyte)[])"M";
+
+        test!("==")(result, decode("TQ"));
+        test!("==")(result, decode("TQ="));
+        test!("==")(result, decode("TQ=="));
+    }
+    // ditto, with a single = required for padding
+    {
+        auto result = cast(Const!(ubyte)[])"Ma";
+
+        test!("==")(result, decode("TWE"));
+        test!("==")(result, decode("TWE="));
+        test!("!=")(result, decode(""));
     }
 }
 
@@ -517,6 +562,9 @@ unittest
     Const!(ubyte)[] payload = cast(Const!(ubyte)[]) str;
     cstring result = encode(payload, false);
     test!("==")(result, "SGVsbG8sIGhvdyBhcmUgeW91IHRvZGF5Pw");
+
+    auto decoded = cast(cstring)decode(result);
+    test!("==")(decoded, str);
 }
 
 

commit 64b109ac800371d0e48f37550be8b471d17fed2b

Don't decode padded chunks with less than two bytes
fd813adfc91c0f56616577d6706db4899cce7917 inadvertently modified the
behaviour of the base64 decoder. Previous to that commit, trailing data of
less than 2 bytes was simply ignored. fd813adfc91c0f56616577d6706db4899cce7917
turned this into an error.

As this was a breaking change in behaviour, this commit restores the original
behaviour: ignoring a single, trailing byte.

src/ocean/util/encode/Base64.d
@@ -322,6 +322,12 @@ body
     previous one), data may or may not contain the trailing padding, and it will
     still be decoded correctly.
 
+    Note: With explicit padding, three padding bytes will cause decoder to
+    throw an Exception. However, in the case of implicit padding, the last byte
+    will be silently ignored if it contains less than two bytes. This was the
+    undocumented behaviour of the decoder, and it will be changed in the next
+    major version.
+
 
     Params:
       Table = The decode table to use. Variadic template parameter is used to
@@ -454,7 +460,7 @@ body
         }
 
         // this will try and decode whatever is left, even if it isn't terminated properly (ie: missing last one or two =)
-        if (paddedPos)
+        if (paddedPos >= 2)
         {
             auto padded = data[($ - paddedPos) .. $];
             assert (padded.length <= 4);
@@ -552,6 +558,22 @@ unittest
         test!("==")(result, decode("TWE"));
         test!("==")(result, decode("TWE="));
         test!("!=")(result, decode(""));
+    }
+
+    // in case when implicit padding is used and the last quadruple
+    // has less than two bytes needed for the decoding in which case
+    // it will be silently ignored
+    {
+        auto result = cast(Const!(ubyte)[])"Maq";
+        // B should be ignored since it's missing the second byte
+        test!("==")(result, decode("TWFxB"));
+    }
+
+    // same as previous test, just there's nothing valid to be
+    // decoded in the first place
+    {
+        ubyte[] empty;
+        test!("==")(empty, decode("T"));
     }
 }
 