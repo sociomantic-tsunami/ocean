bogdan-szabo-sociomantic/ocean-1

Deprecate all locale modules

Build.mak
@@ -20,7 +20,8 @@ TEST_FILTER_OUT += \
 	$C/src/ocean/io/Stdout_tango.d \
 	$C/src/ocean/io/FilePath_tango.d \
 	$C/src/ocean/core/Traits_tango.d \
-	$C/src/ocean/core/Exception_tango.d
+	$C/src/ocean/core/Exception_tango.d \
+	$(shell find $C/src/ocean/text/locale -type f)
 
 # integration test which is disabled by default because it depends on Collectd
 TEST_FILTER_OUT += \

sociomantic-tsunami/ocean

Build.mak
@@ -32,7 +32,8 @@ TEST_FILTER_OUT += \
 	$C/src/ocean/util/log/LayoutChainsaw.d \
 	$C/src/ocean/util/log/Log.d \
 	$C/src/ocean/text/convert/Memory.d \
-	$C/src/ocean/stdc/stringz.d
+	$C/src/ocean/stdc/stringz.d \
+	$(shell find $C/src/ocean/text/locale -type f)
 
 # integration test which is disabled by default because it depends on Collectd
 TEST_FILTER_OUT += \