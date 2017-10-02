4b0ca981efa752978e13b2cef47b65e8733c6448
Mathias Lang <mathias.lang@sociomantic.com>
2017-10-02 18:48:10 +0200

    Remove dead code in TempFile

--- src/ocean/io/device/TempFile.d
+++ src/ocean/io/device/TempFile.d
@@ -32,93 +32,13 @@ import ocean.io.device.Device : Device;
 import ocean.io.device.File;
 import ocean.text.util.StringC;
 
-/******************************************************************************
- ******************************************************************************/
-
-version( Win32 )
-{
-    import ocean.sys.Common : DWORD, LONG, MAX_PATH, PCHAR, CP_UTF8;
-
-    enum : DWORD { FILE_FLAG_OPEN_REPARSE_POINT = 0x00200000 }
-
-    version( Win32SansUnicode )
-    {
-        import ocean.sys.Common :
-            GetVersionExA, OSVERSIONINFO,
-            FILE_FLAG_DELETE_ON_CLOSE,
-            GetTempPathA;
-
-        istring GetTempPath()
-        {
-            auto len = GetTempPathA(0, null);
-            if( len == 0 )
-                throw new Exception("could not obtain temporary path");
-
-            auto result = new char[len+1];
-            len = GetTempPathA(len+1, result.ptr);
-            if( len == 0 )
-                throw new Exception("could not obtain temporary path");
-            return Path.standard(result[0..len]);
-        }
-    }
-    else
-    {
-        import ocean.sys.Common :
-            WideCharToMultiByte,
-            GetVersionExW, OSVERSIONINFO,
-            FILE_FLAG_DELETE_ON_CLOSE,
-            GetTempPathW;
-
-        istring GetTempPath()
-        {
-            auto len = GetTempPathW(0, null);
-            if( len == 0 )
-                throw new Exception("could not obtain temporary path");
-
-            auto result = new wchar[len+1];
-            len = GetTempPathW(len+1, result.ptr);
-            if( len == 0 )
-                throw new Exception("could not obtain temporary path");
-
-            auto dir = new char [len * 3];
-            auto i = WideCharToMultiByte (CP_UTF8, 0, result.ptr, len,
-                                          cast(PCHAR) dir.ptr, dir.length, null, null);
-            return Path.standard (dir[0..i]);
-        }
-    }
-
-    // Determines if reparse points (aka: symlinks) are supported.  Support
-    // was introduced in Windows Vista.
-    bool reparseSupported()
-    {
-        OSVERSIONINFO versionInfo = void;
-        versionInfo.dwOSVersionInfoSize = versionInfo.sizeof;
-
-        void e(){throw new Exception("could not determine Windows version");}
-
-        version( Win32SansUnicode )
-        {
-            if( !GetVersionExA(&versionInfo) ) e();
-        }
-        else
-        {
-            if( !GetVersionExW(&versionInfo) ) e();
-        }
-
-        return (versionInfo.dwMajorVersion >= 6);
-    }
-}
-
-else version( Posix )
-{
-    import core.sys.posix.pwd : getpwnam;
-    import core.sys.posix.unistd : access, getuid, lseek, unlink, W_OK;
-    import ocean.stdc.posix.sys.types : off_t;
-    import ocean.stdc.posix.sys.stat : stat, stat_t;
-    import ocean.stdc.posix.fcntl : O_NOFOLLOW;
-    import core.sys.posix.stdlib : getenv;
-    import ocean.stdc.string : strlen;
-}
+import core.sys.posix.pwd : getpwnam;
+import core.sys.posix.unistd : access, getuid, lseek, unlink, W_OK;
+import ocean.stdc.posix.sys.types : off_t;
+import ocean.stdc.posix.sys.stat : stat, stat_t;
+import ocean.stdc.posix.fcntl : O_NOFOLLOW;
+import core.sys.posix.stdlib : getenv;
+import ocean.stdc.string : strlen;
 
 /******************************************************************************
  *
@@ -163,20 +83,6 @@ else version( Posix )
 
 class TempFile : File
 {
-    /+enum Visibility : ubyte
-    {
-        /**
-         * The temporary file will have read and write access to it restricted
-         * to the current user.
-         */
-        User,
-        /**
-         * The temporary file will have read and write access available to any
-         * user on the system.
-         */
-        World
-    }+/
-
     /**************************************************************************
      *
      * This enumeration is used to control whether the temporary file should
@@ -196,35 +102,6 @@ class TempFile : File
          */
         Permanent
     }
-
-    /+enum Sensitivity : ubyte
-    {
-        /**
-         * Transient files will be truncated to zero length immediately
-         * before closure to prevent casual filesystem inspection to recover
-         * their contents.
-         *
-         * No additional action is taken on permanent files.
-         */
-        None,
-        /**
-         * Transient files will be zeroed-out before truncation, to mask their
-         * contents from more thorough filesystem inspection.
-         *
-         * This option is not compatible with permanent files.
-         */
-        Low
-        /+
-        /**
-         * Transient files will be overwritten first with zeroes, then with
-         * ones, and then with a random 32- or 64-bit pattern (dependant on
-         * which is most efficient.)  The file will then be truncated.
-         *
-         * This option is not compatible with permanent files.
-         */
-        Medium
-        +/
-    }+/
 
     /**************************************************************************
      *
@@ -301,148 +178,94 @@ class TempFile : File
         error("could not create temporary file");
     }
 
-    version( Win32 )
+    private const DEFAULT_LENGTH = 6;
+    private const DEFAULT_PREFIX = ".tmp";
+
+    // Use "~" to work around a bug in DMD where it elides empty constants
+    private const DEFAULT_SUFFIX = "~";
+
+    private const JUNK_CHARS =
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    /***************************************************************************
+     *
+     * Returns the path to the directory where temporary files will be
+     * created.  The returned path is safe to mutate.
+     *
+     **************************************************************************/
+
+    public static istring tempPath()
     {
-        private const DEFAULT_LENGTH = 6;
-        private const DEFAULT_PREFIX = "~t";
-        private const DEFAULT_SUFFIX = ".tmp";
+        // Check for TMPDIR; failing that, use /tmp
+        char* ptr = getenv ("TMPDIR".ptr);
+        if (ptr is null)
+            return "/tmp/";
+        else
+            return idup(ptr[0 .. strlen (ptr)]);
+    }
 
-        private const JUNK_CHARS =
-            "abcdefghijklmnopqrstuvwxyz0123456789";
-
-       /**********************************************************************
-         *
-         * Returns the path to the directory where temporary files will be
-         * created.  The returned path is safe to mutate.
-         *
-         **********************************************************************/
-        public static istring tempPath()
+    /*
+     * Creates a new temporary file at the given path, with the specified
+     * style.
+     */
+    private bool openTempFile(cstring path, TempStyle style)
+    {
+        // Check suitability
         {
-            return GetTempPath;
+            mstring path_mut = Path.parse(path.dup).path;
+            enableStomping(path_mut);
+            auto parentz = StringC.toCString(path_mut);
+
+            // Make sure we have write access
+            if( access(parentz, W_OK) == -1 )
+                error("do not have write access to temporary directory");
+
+            // Get info on directory
+            stat_t sb;
+            if( stat(parentz, &sb) == -1 )
+                error("could not stat temporary directory");
+
+            // Get root's UID
+            auto pwe = getpwnam("root".ptr);
+            if( pwe is null ) error("could not get root's uid");
+            auto root_uid = pwe.pw_uid;
+
+            // Make sure either we or root are the owner
+            if( !(sb.st_uid == root_uid || sb.st_uid == getuid) )
+                error("temporary directory owned by neither root nor user");
+
+            // Check to see if anyone other than us can write to the dir.
+            if( (sb.st_mode & Octal!("22")) != 0 && (sb.st_mode & Octal!("1000")) == 0 )
+                error("sticky bit not set on world-writable directory");
         }
 
-        /*
-         * Creates a new temporary file at the given path, with the specified
-         * style.
-         */
-        private bool openTempFile(cstring path, TempStyle style)
+        // Create file
         {
-            // TODO: Check permissions directly and throw an exception;
-            // otherwise, we could spin trying to make a file when it's
-            // actually not possible.
-
             Style filestyle = {Access.ReadWrite, Open.New,
                                Share.None, Cache.None};
 
-            DWORD attr;
+            auto addflags = O_NOFOLLOW;
 
-            // Set up flags
-            attr = reparseSupported ? FILE_FLAG_OPEN_REPARSE_POINT : 0;
-            if( style.transience == Transience.Transient )
-                attr |= FILE_FLAG_DELETE_ON_CLOSE;
-
-            if (!super.open (path, filestyle, attr))
+            if (!super.open(path, filestyle, addflags, Octal!("600")))
                 return false;
 
+            if( style.transience == Transience.Transient )
+            {
+                // BUG TODO: check to make sure the path still points
+                // to the file we opened.  Pity you can't unlink a file
+                // descriptor...
+
+                // NOTE: This should be an exception and not simply
+                // returning false, since this is a violation of our
+                // guarantees.
+                if( unlink((path ~ "\0").ptr) == -1 )
+                    error("could not remove transient file");
+            }
+
             _style = style;
+
             return true;
         }
-    }
-    else version( Posix )
-    {
-        private const DEFAULT_LENGTH = 6;
-        private const DEFAULT_PREFIX = ".tmp";
-
-        // Use "~" to work around a bug in DMD where it elides empty constants
-        private const DEFAULT_SUFFIX = "~";
-
-        private const JUNK_CHARS =
-            "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-            ~ "abcdefghijklmnopqrstuvwxyz0123456789";
-
-       /**********************************************************************
-         *
-         * Returns the path to the directory where temporary files will be
-         * created.  The returned path is safe to mutate.
-         *
-         **********************************************************************/
-        public static istring tempPath()
-        {
-            // Check for TMPDIR; failing that, use /tmp
-            char* ptr = getenv ("TMPDIR".ptr);
-            if (ptr is null)
-                return "/tmp/";
-            else
-                return idup(ptr[0 .. strlen (ptr)]);
-        }
-
-        /*
-         * Creates a new temporary file at the given path, with the specified
-         * style.
-         */
-        private bool openTempFile(cstring path, TempStyle style)
-        {
-            // Check suitability
-            {
-                mstring path_mut = Path.parse(path.dup).path;
-                enableStomping(path_mut);
-                auto parentz = StringC.toCString(path_mut);
-
-                // Make sure we have write access
-                if( access(parentz, W_OK) == -1 )
-                    error("do not have write access to temporary directory");
-
-                // Get info on directory
-                stat_t sb;
-                if( stat(parentz, &sb) == -1 )
-                    error("could not stat temporary directory");
-
-                // Get root's UID
-                auto pwe = getpwnam("root".ptr);
-                if( pwe is null ) error("could not get root's uid");
-                auto root_uid = pwe.pw_uid;
-
-                // Make sure either we or root are the owner
-                if( !(sb.st_uid == root_uid || sb.st_uid == getuid) )
-                    error("temporary directory owned by neither root nor user");
-
-                // Check to see if anyone other than us can write to the dir.
-                if( (sb.st_mode & Octal!("22")) != 0 && (sb.st_mode & Octal!("1000")) == 0 )
-                    error("sticky bit not set on world-writable directory");
-            }
-
-            // Create file
-            {
-                Style filestyle = {Access.ReadWrite, Open.New,
-                                   Share.None, Cache.None};
-
-                auto addflags = O_NOFOLLOW;
-
-                if (!super.open(path, filestyle, addflags, Octal!("600")))
-                    return false;
-
-                if( style.transience == Transience.Transient )
-                {
-                    // BUG TODO: check to make sure the path still points
-                    // to the file we opened.  Pity you can't unlink a file
-                    // descriptor...
-
-                    // NOTE: This should be an exception and not simply
-                    // returning false, since this is a violation of our
-                    // guarantees.
-                    if( unlink((path ~ "\0").ptr) == -1 )
-                        error("could not remove transient file");
-                }
-
-                _style = style;
-
-                return true;
-            }
-        }
-    }
-    else
-    {
-        static assert(false, "Unsupported platform");
     }
 
     /*
@@ -469,72 +292,4 @@ class TempFile : File
         static assert( !is(Sensitivity) );
         super.detach();
     }
-}
-
-version( TempFile_SelfTest ):
-
-import ocean.io.Console : Cin;
-import ocean.io.Stdout : Stdout;
-
-void main()
-{
-    Stdout(r"
-Please ensure that the transient file no longer exists once the TempFile
-object is destroyed, and that the permanent file does.  You should also check
-the following on both:
-
- * the file should be owned by you,
- * the owner should have read and write permissions,
- * no other permissions should be set on the file.
-
-For POSIX systems:
-
- * the temp directory should be owned by either root or you,
- * if anyone other than root or you can write to it, the sticky bit should be
-   set,
- * if the directory is writable by anyone other than root or the user, and the
-   sticky bit is *not* set, then creating the temporary file should fail.
-
-You might want to delete the permanent one afterwards, too. :)")
-    .newline;
-
-    Stdout.formatln("Creating a transient file:");
-    {
-        scope tempFile = new TempFile(/*TempFile.UserPermanent*/);
-
-        Stdout.formatln(" .. path: {}", tempFile);
-
-        tempFile.write("Transient temp file.");
-
-        auto buffer = new char[1023];
-        tempFile.seek(0);
-        buffer = buffer[0..tempFile.read(buffer)];
-
-        Stdout.formatln(" .. contents: \"{}\"", buffer);
-
-        Stdout(" .. press Enter to destroy TempFile object.").newline;
-        Cin.copyln();
-    }
-
-    Stdout.newline;
-
-    Stdout.formatln("Creating a permanent file:");
-    {
-        scope tempFile = new TempFile(TempFile.Permanent);
-
-        Stdout.formatln(" .. path: {}", tempFile);
-
-        tempFile.write("Permanent temp file.");
-
-        auto buffer = new char[1023];
-        tempFile.seek(0);
-        buffer = buffer[0..tempFile.read(buffer)];
-
-        Stdout.formatln(" .. contents: \"{}\"", buffer);
-
-        Stdout(" .. press Enter to destroy TempFile object.").flush;
-        Cin.copyln();
-    }
-
-    Stdout("\nDone.").newline;
 }
