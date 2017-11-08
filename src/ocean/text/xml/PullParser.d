6bb073228f1dcb8d180e7738326b423422b65410
Don Clugston <don.clugston@sociomantic.com>
2017-11-08 19:05:06 +0100

    Fix the broken D2 conversion of PullParser
    PullParser was converted to D2 incorrectly. Internally, the code does not
    modify the text which is being parsed. But the D2 conversion incorrectly used
    an `mstring` to store the text being parsed. This required a memory
    allocation for every cstring which was parsed, making the code essentially
    unusable in D2.
    
    The `name` and `value` member functions were incorrectly marked as returning
    mutable strings. To entirely avoid breaking changes, that is is not fixed in
    this PR, but the documentation now states that they may allocate memory.

--- src/ocean/text/xml/PullParser.d
+++ src/ocean/text/xml/PullParser.d
@@ -94,9 +94,9 @@ public enum XmlTokenType {Done, StartElement, Attribute, EndElement,
 class PullParser(Ch = char)
 {
         public int                      depth;
-        public Ch[]                     prefix;
-        public Ch[]                     rawValue;
-        public Ch[]                     localName;
+        public Const!(Ch)[]             prefix;
+        public Const!(Ch)[]             rawValue;
+        public Const!(Ch)[]             localName;
         public XmlTokenType             type = XmlTokenType.None;
 
         package XmlText!(Ch)            text;
@@ -499,7 +499,7 @@ version (partialwhite)
 
         ***********************************************************************/
 
-        private XmlTokenType doUnexpected (istring msg, Ch* p)
+        private XmlTokenType doUnexpected (istring msg, Const!(Ch)* p)
         {
                 return position ("parse error :: unexpected  " ~ msg, p);
         }
@@ -508,7 +508,7 @@ version (partialwhite)
 
         ***********************************************************************/
 
-        private XmlTokenType doExpected (istring msg, Ch* p)
+        private XmlTokenType doExpected (istring msg, Const!(Ch)* p)
         {
                 char[6] tmp = void;
                 return position ("parse error :: expected  " ~ msg ~ " instead of "
@@ -519,7 +519,7 @@ version (partialwhite)
 
         ***********************************************************************/
 
-        private XmlTokenType position (istring msg, Ch* p)
+        private XmlTokenType position (istring msg, Const!(Ch)* p)
         {
                 return error (msg ~ " at position "
                     ~ idup(Integer.toString(p-text.text.ptr)));
@@ -539,16 +539,28 @@ version (partialwhite)
 
                 Return the raw value of the current token
 
+                This function may allocate memory. It is always better to
+                use rawValue instead.
+
         ***********************************************************************/
 
         final Ch[] value()
         {
-                return rawValue;
+                version (D_Version2)
+                {
+                    return rawValue.dup;
+                }
+                else
+                {
+                    return rawValue;
+                }
         }
 
         /***********************************************************************
 
                 Return the name of the current token
+
+                This function may allocate memory.
 
         ***********************************************************************/
 
@@ -556,7 +568,15 @@ version (partialwhite)
         {
                 if (prefix.length)
                     return prefix ~ ":" ~ localName;
-                return localName;
+
+                version (D_Version2)
+                {
+                    return localName.dup;
+                }
+                else
+                {
+                    return localName;
+                }
         }
 
         /***********************************************************************
@@ -589,22 +609,10 @@ version (partialwhite)
 
         ***********************************************************************/
 
-        final void reset(Ch[] newText)
+        final void reset(Const!(Ch)[] newText)
         {
                 text.reset (newText);
                 reset_;
-        }
-
-        version (D_Version2)
-        {
-            // adding this as versioned overload to avoid extra allocation
-            // for mutable argument
-
-            final void reset(Const!(Ch)[] newText)
-            {
-                    text.reset (newText.dup);
-                    reset_;
-            }
         }
 
         /***********************************************************************
@@ -665,12 +673,12 @@ version (partialwhite)
 
 package struct XmlText(Ch)
 {
-        package Ch*     end;
+        package Const!(Ch)* end;
         package size_t  len;
-        package Ch[]    text;
-        package Ch*     point;
+        package Const!(Ch)[] text;
+        package Const!(Ch)* point;
 
-        final void reset(Ch[] newText)
+        final void reset(Const!(Ch)[] newText)
         {
                 this.text = newText;
                 this.len = newText.length;
@@ -703,6 +711,8 @@ package struct XmlText(Ch)
 
 version (UnitTest)
 {
+    import ocean.core.Test;
+
 	/***********************************************************************
 
 	***********************************************************************/
@@ -770,4 +780,12 @@ unittest
 {
     auto itr = new PullParser!(char)(testXML);
     testParser (itr);
+
+    // Parsing new text (or even the same one) should not involve any further
+    // memory allocation
+
+    testNoAlloc({
+      itr.reset(testXML);
+      testParser(itr);
+    }());
 }
