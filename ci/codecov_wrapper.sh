e599535aab3379d9d598c2c94b1cf1b3c9f607ee
Mihails Strasuns <mihails.strasuns.contractor@sociomantic.com>
2017-12-12 15:53:13 +0200

    Use tmp dir setup for codecov

--- ci/codecov_wrapper.sh
+++ ci/codecov_wrapper.sh
@@ -10,4 +10,15 @@ codecov_vars="$(printenv -0 | sed -zn 's/^\(CODECOV_[^=]\+\)=.*$/\1\n/p' |
 dlang_vars="DIST DMD DC DVER D2_ONLY F V"
 export BEAVER_DOCKER_VARS="${BEAVER_DOCKER_VARS:-} $codecov_vars $dlang_vars"
 
-beaver run ci/codecov.sh
+# Copy coverage reports and git structure to a clean directory
+tmp=`mktemp -d`
+trap 'r=$?; rm -fr "$tmp"; exit $r' EXIT INT TERM QUIT
+mkdir -p "$tmp/reports"
+cp -a .*.lst "$tmp/reports"
+git archive --format=tar HEAD | (cd "$tmp" && tar xf -)
+cp -a .git "$tmp/"
+cp -av "ci/codecov.sh" "$tmp/codecov"
+
+cd $tmp
+beaver run ./codecov.sh -n beaver -s reports -e DIST,DMD,DC,F -X gcov -X \
+    coveragepy -X xcode
