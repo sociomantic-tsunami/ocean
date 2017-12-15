1c2c690bdca7dbe418b7359bbfed3e606c20ff83
Mihails Strasuns <mihails.strasuns.contractor@sociomantic.com>
2017-12-15 10:38:25 +0200

    Upgrade to supported makd version
    Removing Config.mak, per Leandro explanation:
    
    Build.mak, if you need to use other variables normally you want to do it
    in Build.mak. Config.mak is only for setting defaults not depending on
    other variables, as there is almost nothing defined when Config.mak is
    processed. Config.local.mak is the same but for local configuration that
    you don't want to check in, for example if you like to compile always in
    verbose mode, set V :=1 in Config.local.mak.

--- Build.mak
+++ Build.mak
@@ -1,3 +1,14 @@
+# Makd auto-detects if Ocean's test runner should be used based on submodules,
+# but we -of course- don't have Ocean as a submodule, so we set it explicitly.
+TEST_RUNNER_MODULE := ocean.core.UnitTestRunner
+
+# This will make D2 unittests fail if stomping prevention is triggered
+# (only with dmd-transitional, no effect on other compilers)
+export ASSERT_ON_STOMPING_PREVENTION=1
+
+# Common D compiler flags
+override DFLAGS += -w -version=GLIBC
+
 # Treat deprecations as errors to ensure ocean doesn't use own deprecated
 # symbols internally. Disable it on explicit flag to make possible regression
 # testing in D upstream.
@@ -8,10 +19,6 @@ ifneq ($(ALLOW_DEPRECATIONS),1)
 	override DFLAGS := $(filter-out -di,$(DFLAGS))
 	endif
 endif
-
-# Makd auto-detects if Ocean's test runner should be used based on submodules,
-# but we -or course- don't have Ocean as a submodule, so we set it explicitly.
-TEST_RUNNER_MODULE := ocean.core.UnitTestRunner
 
 # Remove coverage files
 clean += .*.lst
@@ -50,7 +57,7 @@ TEST_FILTER_OUT += \
 
 # integration test which is disabled by default because it depends on Collectd
 TEST_FILTER_OUT += \
-	$C/test/collectd/main.d
+	$C/integrationtest/collectd/main.d
 
 $O/test-%: override LDFLAGS += -lebtree
 
@@ -61,5 +68,8 @@ $O/test-httpserver: override LDFLAGS += -lglib-2.0
 # Link unittests to all used libraries
 $O/%unittests: override LDFLAGS += -lglib-2.0 -lpcre -lxml2 -lxslt -lebtree \
 		-lreadline -lhistory -llzo2 -lbz2 -lz -ldl -lgcrypt -lgpg-error -lrt
-# Enable coverage generation from unittests
-$O/%unittests: override DFLAGS += $(COVFLAG)
+
+# Enable coverage report in CI
+ifdef CI
+$O/%unittests: override DFLAGS += -cov
+endif
---
+++ integrationtest/scheduler/main.d
@@ -0,0 +1,114 @@
+/*******************************************************************************
+
+    Test project doing emulating scheduler based application different kind of
+    concurrent task processing. Most useful when doing manual debugging with
+    `-debug=TaskScheduler` trace enabled, as there won't be any extra noise from
+    imported unittests.
+
+    Copyright:
+        Copyright (c) 2017 sociomantic Labs GmbH.
+        All rights reserved.
+
+    License:
+        Boost Software License Version 1.0. See LICENSE_BOOST.txt for details.
+        Alternatively, this file may be distributed under the terms of the Tango
+        3-Clause BSD License (see LICENSE_BSD.txt for details).
+
+*******************************************************************************/
+
+module integrationtest.scheduler.main;
+
+import ocean.task.Scheduler;
+import ocean.task.Task;
+import ocean.task.TaskPool;
+import ocean.task.util.Timer;
+
+import ocean.transition;
+import ocean.core.Test;
+import ocean.io.Stdout;
+
+import core.stdc.stdlib;
+
+/// Entry point task that starts up everything else
+class MainTask : Task
+{
+    GeneratorTask[] generators;
+    TaskPool!(RegularTask) pool;
+
+    this ( )
+    {
+        this.pool = new typeof(this.pool);
+
+        for (int i = 0; i < 5; ++i)
+        {
+            this.generators ~= new GeneratorTask;
+            this.generators[$-1].root = this;
+        }
+    }
+
+    override public void run ( )
+    {
+        foreach (task; this.generators)
+            theScheduler.schedule(task);
+    }
+}
+
+/// Task emulating some sort of priority / real-time computation
+/// that must not ever wait in queue, but is known to have predictable
+/// max concurrency factor. Spawns `RegularTask` as part of its work.
+class GeneratorTask : Task
+{
+    MainTask root;
+
+    override public void run ( )
+    {
+        for (int i = 0; i < 5; ++i)
+            this.root.pool.start();
+    }
+}
+
+/// Task doing some prolonged I/O wait after being queued
+class RegularTask : Task
+{
+    static long completed_count;
+
+    override public void run ( )
+    {
+        .wait(100_000);
+        ++RegularTask.completed_count;
+    }
+
+    void copyArguments ( ) { }
+}
+
+version(UnitTest) {} else
+void main ( )
+{
+    SchedulerConfiguration config;
+
+    with (config)
+    {
+        worker_fiber_stack_size = 102400;
+        worker_fiber_limit = 10;
+        task_queue_limit = 100;
+        suspended_task_limit = 1000;
+
+        specialized_pools = [
+            PoolDescription(GeneratorTask.classinfo.name, 204800)
+        ];
+    }
+
+    initScheduler(config);
+
+    theScheduler.exception_handler = ( Task t, Exception e )
+    {
+        Stderr.formatln("{} [{}:{}] {}", e.classinfo.name,
+            e.file, e.line, getMsg(e));
+        abort();
+    };
+
+    theScheduler.schedule(new MainTask);
+    theScheduler.eventLoop();
+
+    test!("==")(RegularTask.completed_count, 25);
+}
