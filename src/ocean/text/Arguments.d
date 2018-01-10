sociomantic-tsunami/ocean

commit 4692ecaa386789300b512c5a515445be72db6bba

Deprecate RTTI-specific functions in stream's Format
We cannot simply turn 'format' from RT args to CT args in v4.0.0, because that would try to format the TypeInfo[] and void*[],
and thus might introduce hard to spot bugs. Instead, with this extra deprecation, we'll catch any remaining user of the
functionality.

src/ocean/text/Arguments.d
@@ -1271,7 +1271,7 @@ public class Arguments
 
     public void displayErrors ( FormatOutput!(char) output = Stderr )
     {
-        output(this.errors());
+        output.format("{}", this.errors());
     }
 
 

src/ocean/core/UnitTestRunner.d
@@ -474,7 +474,7 @@ private scope class UnitTestRunner
                 output.flush();
                 output.close();
             }
-            output(printer.print(this.xml_doc)).newline;
+            output.formatln("{}", printer.print(this.xml_doc));
         }
         catch (Exception e)
         {
@@ -897,7 +897,7 @@ private scope class UnitTestRunner
     private void printHelp ( FormatOutput!(char) output )
     {
         this.printUsage(output);
-        output.print(`
+        output.format(`
 optional arguments:
   -h, --help        print this message and exit
   -v, --verbose     print more information about unittest progress, can be

src/ocean/io/Stdout.d
@@ -57,10 +57,8 @@ public alias Stderr stderr; /// Alternative.
 
 static this ( )
 {
-    auto layout = Layout!(char).instance;
-
-    Stdout = new TerminalOutput!(char)(layout, Cout.stream);
-    Stderr = new TerminalOutput!(char)(layout, Cerr.stream);
+    Stdout = new TerminalOutput!(char)(Cout.stream);
+    Stderr = new TerminalOutput!(char)(Cerr.stream);
 
     Stdout.flush = !Cout.redirected;
     Stdout.redirect = Cout.redirected;
@@ -121,7 +119,7 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     public this (OutputStream output, Immut!(T)[] eol = Eol)
     {
-        this (Layout!(T).instance, output, eol);
+        super(output, eol);
     }
 
 
@@ -132,6 +130,7 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     ***************************************************************************/
 
+    deprecated("Using Layout with this class is deprecated - Remove first argument at call site")
     public this (Layout!(T) convert, OutputStream output, Immut!(T)[] eol = Eol)
     {
         super(convert, output, eol);
@@ -146,7 +145,7 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     public typeof(this) format ( Const!(T)[] fmt, ... )
     {
-        super.format(fmt, _arguments, _argptr);
+        super._transitional_format(fmt, _arguments, _argptr);
         return this;
     }
 
@@ -158,6 +157,7 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     ***************************************************************************/
 
+    deprecated("RTTI-specific function is deprecated, use a Formatter-compatible API")
     public typeof(this) format (Const!(T)[] fmt, TypeInfo[] arguments, ArgList args)
     {
         super.format(fmt, arguments, args);
@@ -174,8 +174,8 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     public typeof(this) formatln ( Const!(T)[] fmt, ... )
     {
-        super.formatln(fmt, _arguments, _argptr);
-        return this;
+        super._transitional_format(fmt, _arguments, _argptr);
+        return this.newline;
     }
 
 
@@ -186,6 +186,7 @@ public class TerminalOutput ( T ) : FormatOutput!(T)
 
     ***************************************************************************/
 
+    deprecated("RTTI-specific function is deprecated, use a Formatter-compatible API")
     public typeof(this) formatln (Const!(T)[] fmt, TypeInfo[] arguments, ArgList args)
     {
         super.formatln(fmt, arguments, args);

src/ocean/io/stream/Format.d
@@ -44,15 +44,6 @@ version(DigitalMars)
         ---
         auto print = new FormatOutput!(char) (...);
 
-        print ("hello");                    // => hello
-        print (1);                          // => 1
-        print (3.14);                       // => 3.14
-        print ('b');                        // => b
-        print (1, 2, 3);                    // => 1, 2, 3
-        print ("abc", 1, 2, 3);             // => abc, 1, 2, 3
-        print ("abc", 1, 2) ("foo");        // => abc, 1, 2foo
-        print ("abc") ("def") (3.14);       // => abcdef3.14
-
         print.format ("abc {}", 1);         // => abc 1
         print.format ("abc {}:{}", 1, 2);   // => abc 1:2
         print.format ("abc {1}:{0}", 1, 2); // => abc 2:1
@@ -63,13 +54,8 @@ version(DigitalMars)
         are several use-cases where dropping an argument is legitimate,
         so we're currently not enforcing any particular trap mechanism.
 
-        Flushing the output is achieved through the flush() method, or
-        via an empty pair of parens:
+        Flushing the output is achieved through the flush() method:
         ---
-        print ("hello world") ();
-        print ("hello world").flush;
-
-        print.format ("hello {}", "world") ();
         print.format ("hello {}", "world").flush;
         ---
 
@@ -79,7 +65,6 @@ version(DigitalMars)
         newlines are generated instead via the newline() method, which also
         flushes the output when configured to do so:
         ---
-        print ("hello ") ("world").newline;
         print.format ("hello {}", "world").newline;
         print.formatln ("hello {}", "world");
         ---
@@ -110,7 +95,10 @@ class FormatOutput(T) : OutputFilter
 
         this (OutputStream output, Const!(T)[] eol = Eol)
         {
-                this (Layout!(T).instance, output, eol);
+            this.convert = Layout!(T).instance;
+            this.eol = eol;
+            super (output);
+
         }
 
         /**********************************************************************
@@ -120,6 +108,7 @@ class FormatOutput(T) : OutputFilter
 
         **********************************************************************/
 
+        deprecated("Using Layout with this class is deprecated - Remove the first argument at call site")
         this (Layout!(T) convert, OutputStream output, Const!(T)[] eol = Eol)
         {
                 verify(convert !is null);
@@ -138,7 +127,14 @@ class FormatOutput(T) : OutputFilter
 
         final FormatOutput format (Const!(T)[] fmt, ...)
         {
-            return format(fmt, _arguments, _argptr);
+            this.convert(&this.emit, _arguments, _argptr, fmt);
+            return this;
+        }
+
+        /// Used by derived classes to avoid deprecations - deprecated, remove in v4.0.0
+        protected final void _transitional_format (Const!(T)[] fmt, TypeInfo[] arguments, ArgList args)
+        {
+            this.convert(&this.emit, arguments, args, fmt);
         }
 
         /**********************************************************************
@@ -148,6 +144,7 @@ class FormatOutput(T) : OutputFilter
 
         **********************************************************************/
 
+        deprecated("RTTI-specific function is deprecated, use a Formatter-compatible API")
         final FormatOutput format (Const!(T)[] fmt, TypeInfo[] arguments, ArgList args)
         {
             convert (&emit, arguments, args, fmt);
@@ -163,7 +160,8 @@ class FormatOutput(T) : OutputFilter
 
         final FormatOutput formatln (Const!(T)[] fmt, ...)
         {
-            return formatln(fmt, _arguments, _argptr);
+            this.convert(&this.emit, _arguments, _argptr, fmt);
+            return this.newline;
         }
 
         /**********************************************************************
@@ -172,7 +170,7 @@ class FormatOutput(T) : OutputFilter
                 pass-through version.
 
         **********************************************************************/
-
+        deprecated("RTTI-specific function is deprecated, use a Formatter-compatible API")
         final FormatOutput formatln (Const!(T)[] fmt, TypeInfo[] arguments, ArgList args)
         {
             convert (&emit, arguments, args, fmt);
@@ -187,6 +185,7 @@ class FormatOutput(T) : OutputFilter
 
         **********************************************************************/
 
+        deprecated("Use the 'format' method instead, or 'flush'")
         final FormatOutput print ( ... )
         {
                 static slice =  "{}, {}, {}, {}, {}, {}, {}, {}, " ~
@@ -261,6 +260,7 @@ class FormatOutput(T) : OutputFilter
 
         **********************************************************************/
 
+        deprecated("FormatOutput will stop using Layout and switch to Formatter in v4.0.0")
         final Layout!(T) layout ()
         {
                 return convert;
@@ -271,7 +271,7 @@ class FormatOutput(T) : OutputFilter
                 Set the associated Layout.
 
         **********************************************************************/
-
+        deprecated("FormatOutput will stop using Layout and switch to Formatter in v4.0.0")
         final FormatOutput layout (Layout!(T) layout)
         {
                 convert = layout;

src/ocean/util/app/ext/ArgumentsExt.d
@@ -190,7 +190,7 @@ class ArgumentsExt : IApplicationExtension
             args.displayErrors(this.stderr);
             foreach (error; errors)
             {
-                this.stderr(error).newline;
+                this.stderr.format("{}", error).newline;
             }
             if (ocean_stderr !is null)
                 ocean_stderr.default_colour;

commit 4ce1f8f08a93050c50f7c9f228a05609ca9eda56

Fix 429: No error message when command line argument has error
The regression appeared in #362, specifically in commit 480fb9fcd71a89e8e93fe917937fda258b08a5aa.
The change in this commit was wrong, it used `sprint` which is equivalent to `snformat`,
(which write to a buffer up to the buffer's size,
when all we really wanted was something that appended to the buffer.

src/ocean/text/Arguments.d
@@ -1134,7 +1134,7 @@ public class Arguments
             if (arg.error)
             {
                 // TODO: Replace with `sformat` in v4.0.0
-                ocean.text.convert.Layout_tango.Layout!(char).instance.sprint(
+                ocean.text.convert.Layout_tango.Layout!(char).format(
                     result, msgs[arg.error-1], arg.name,
                     arg.values.length, arg.min, arg.max, arg.bogus,
                     arg.options);
@@ -2607,6 +2607,7 @@ unittest
     x.requires('y');
     test(args.clear.parse("-xy"));
     test(args.clear.parse("-xz") is false);
+    test!("==")(args.errors(), "argument 'x' requires 'y'\n");
 
     // defaults
     z.defaults("foo");