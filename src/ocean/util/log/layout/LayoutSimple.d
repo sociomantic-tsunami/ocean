98b79fc187da95ce8b19d19a88c5b7a938fce2e4
Mathias Lang <mathias.lang@sociomantic.com>
2018-01-16 13:02:08 +0100

    Unify Appender.Layout.format delegate type with the rest of Ocean
    Since `size_t` delegate were little or not used (for various, valid reasons),
    we moved away from them.
    Moreover, there's no point in accepting a `Const!(void)[]` if the data is casted under the hood,
    as it essentially relies on the data being `cstring` without the safety of type checks.
    For those reasons, the delegate types were updates.
    This is a breaking change which should have little impact on downstream, if any.

--- src/ocean/util/log/layout/LayoutSimple.d
+++ src/ocean/util/log/layout/LayoutSimple.d
@@ -64,7 +64,7 @@ public class LayoutSimple : Appender.Layout
 
     ***************************************************************************/
 
-    public override void format (LogEvent event, size_t delegate(Const!(void)[]) dg)
+    public override void format (LogEvent event, void delegate(cstring) dg)
     {
         auto level = event.levelName;
 
