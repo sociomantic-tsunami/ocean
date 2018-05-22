8da7d28f7d8cd5e3152c41d327b708e251350dd5
Mihails Strasuns <mihails.strasuns.contractor@sociomantic.com>
2018-05-22 13:50:57 +0300

    Refactor CI script to use `install` section

--- ci/closures.sh
+++ ci/closures.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -xe
 
-# Env variables needed for `beaver dlang` commands
-export DMD=2.071.2.s*
-export F=devel
 # Enables printing of all potential GC allocation sources to stdout
 export DFLAGS=-vgc
 
-# Prepare docker image and sources
-beaver dlang install
+# Prepare sources
 beaver dlang make d2conv
 
 # Ensure that there are no lines about closure allocations in the output
