d0b6c5ebd9f308fcefdb3258d1cf4bc335974bb1
Matthias Wende <matthias.wende@sociomantic.com>
2018-10-09 11:02:50 +0200

    Enable stomping for suspendables array

--- src/ocean/io/model/ISuspendableThrottler.d
+++ src/ocean/io/model/ISuspendableThrottler.d
@@ -162,6 +162,7 @@ abstract public class ISuspendableThrottler
     {
         this.suspendables =
             this.suspendables[0 .. this.suspendables.moveToEnd(s)];
+        enableStomping(suspendables);
     }
 
 
