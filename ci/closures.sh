528b644794a5ac7c82a88839ec3841fea1b1fc53
Joseph Rushton Wakeling <joseph.wakeling@sociomantic.com>
2018-10-26 14:07:42 +0200

    Ensure closures script fails if make fails
    The intent behind this version of the script is that it should fail if
    `make` fails, if `grep` fails, or if `grep` finds a matching line; in
    other words, if the exit status of `make` is non-zero or that of `grep`
    is not 1 (no lines found).
    
    The `set -o pipeline` statement in the original script means non-zero
    exit statuses from `make` propagate to the exit status of `grep`, but
    the leading `!` means this is inverted to result in a false test pass.
    
    Applying the `!` only to the output status of `grep` itself would fix
    this, but leaves a second problem: `grep` may itself fail, in which case
    it will report exit status 2.
    
    This patch therefore rewrites the check to use `test` to ensure that the
    exit status of `grep` is 1.  This means we can no longer use `set -e` as
    this would mean that the `grep` command would fail: instead we carry out
    manual checks on the exit status of each command.
    
    In order to simplify the script, we replace the `make | grep` pipe with
    a multi-line version where `make` output is written to a tempfile.  This
    allows us to drop `set -o pipeline`.
    
    This should mean that the script will cause a test failure in any of the
    following circumstances:
    
      * `make` fails (exit status non-zero) for either of the 2 calls
      * `grep` fails (exit status 2)
      * `grep` finds at least one line containing `closure` (exit status 0)
    
    ... which in all cases will result in the script exiting with status 1.
    If on the other hand `make` passes and `grep` does not find `closure`,
    we will get the zero exit status required to pass the test.
    
    Note that this update may mean that the closure script will now fail for
    `+d2` auto-converted releases: the hardcoded `make d2conv` means that it
    will run `d1to2fix` on already-converted code, and thus `make fasttest`
    will fail.

--- ci/closures.sh
+++ ci/closures.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
-set -xe
-set -o pipefail
+set -x
 
 # Enables printing of all potential GC allocation sources to stdout
 export DFLAGS=-vgc
 
 # Prepare sources
-beaver dlang make d2conv
+beaver dlang make d2conv || exit 1
 
-# Ensure that there are no lines about closure allocations in the output
-! beaver dlang make fasttest 2>&1 | grep "closure"
+# Run tests and write compiler output to temporary file
+compiler_output=`mktemp`
+beaver dlang make fasttest 2>&1 > $compiler_output || exit 1
+
+# Ensure there are no lines about closure allocations in the output.
+# Note explicit check for `grep` exit status 1, i.e. no lines found.
+grep -e "closure" $compiler_output
+test $? -eq 1 || exit 1
